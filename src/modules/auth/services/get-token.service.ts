cca6a22d6a40372337edddbb73f42fce43df0292
João Victor Martins <j_victorfernandes@hotmail.com>
2025-04-01 15:40:18 -0300

    feat: atualiza a duração dos tokens de acesso e refresh para 1 hora e 7 dias, respectivamente

--- src/modules/auth/services/get-token.service.ts
+++ src/modules/auth/services/get-token.service.ts
@@ -19,17 +19,23 @@ export class GetTokenService implements IGetTokenService {
   async execute(getToken: IGetToken): Promise<IGetTokenRetun> {
     const { userId, email } = getToken;
     const payload = { sub: userId, email: email };
+
     const [token, refreshToken] = await Promise.all([
-      this.jwtService.signAsync(payload),
       this.jwtService.signAsync(payload, {
         secret: process.env.JWT_SECRET,
-        expiresIn: '15m',
+        expiresIn: '1h',
+      }),
+      this.jwtService.signAsync(payload, {
+        secret: process.env.JWT_SECRET,
+        expiresIn: '7d',
       }),
     ]);
+
     await Promise.all([
       this.authRepository.updateRefreshToken(userId, refreshToken),
       this.authRepository.updateToken(userId, token),
     ]);
+
     return { token, refreshToken };
   }
 }
